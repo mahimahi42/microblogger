be736027679504eba0cb9a496e0faa07e0a8731f
Bryce Davis <bad1128@gmail.com>
2014-04-01 09:34:43 -0400

    Add command to get last tweet of all followers to run method

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -30,6 +30,30 @@ class MicroBlogger
         end
     end
 
+    def followers_list
+        screen_names = []
+        @client.followers.users.each do |user|
+            screen_names << user["screen_name"]
+        end
+        return screen_names
+    end
+
+    def spam_my_followers(message)
+        followers = self.followers_list
+        followers.each do |f|
+            self.dm(f, message)
+        end
+    end
+
+    def everyones_last_tweet
+        friends = @client.friends
+        friends.each do |friend|
+            puts "#{friend.screen_name} said..."
+            puts "#{friend.status.text}"            
+            puts ""
+        end
+    end
+
     def run
         puts "Welcome to the JSL Twitter Client!"
         command = ""
@@ -41,7 +65,9 @@ class MicroBlogger
             case command
                 when "q" then puts "Goodbye!"
                 when "t" then self.tweet(parts[1..-1].join(" "))
-                when "dm" then dm(parts[1], parts[2..-1].join(" "))
+                when "dm" then self.dm(parts[1], parts[2..-1].join(" "))
+                when "spam" then self.spam(parts[1..-1].join(" "))
+                when "elt" then self.everyones_last_tweet
                 else
                     puts "Sorry, I don't know how to #{command}"
             end
