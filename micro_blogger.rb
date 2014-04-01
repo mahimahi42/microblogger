b361a01dd7db33dc7cb2b9083063e2786a851e8f
Bryce Davis <bad1128@gmail.com>
2014-04-01 09:21:27 -0400

    Add tweet command to run method

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -21,7 +21,15 @@ class MicroBlogger
         command = ""
         while command != "q"
             printf "enter command: "
-            command = gets.chomp
+            input = gets.chomp
+            parts = input.split(" ")
+            command = parts[0]
+            case command
+                when "q" then puts "Goodbye!"
+                when "t" then self.tweet(parts[1..-1].join(" "))
+                else
+                    puts "Sorry, I don't know how to #{command}"
+            end
         end
     end
 end
